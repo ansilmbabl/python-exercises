b7a3365c93947c4ff3255cab0971016aaac1527e
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-18 16:22:51 +0530

    Create 11.Min-maxing.py
    challenge 11

---
+++ python challenges by  Python Principles/11.Min-maxing.py
@@ -0,0 +1,38 @@
+"""
+Min-maxing
+Define a function named largest_difference that takes a list of numbers as its only parameter.
+
+Your function should compute and return the difference between the largest and smallest number in the list.
+
+For example, the call largest_difference([1, 2, 3]) should return 2 because 3 - 1 is 2.
+
+You may assume that no numbers are smaller or larger than -100 and 100.
+"""
+
+#my solution.......................
+def largest_difference(l):
+    return max(l)-min(l)
+  
+...................................
+
+#solution.............................
+# short solution
+def largest_difference(numbers):
+    return max(numbers) - min(numbers)
+
+# naive solution
+def largest_difference(numbers):
+    smallest = 100
+    for n in numbers:
+        if n < smallest:
+            smallest = n
+
+    largest = -100
+    for n in numbers:
+        if n > largest:
+            largest = n
+
+    difference = largest - smallest
+    return difference
+  
+........................................
