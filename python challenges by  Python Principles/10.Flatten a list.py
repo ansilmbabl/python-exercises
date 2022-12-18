88d8b716e1d3ad7f81f9db593d87ec6e3a97061d
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-18 15:03:11 +0530

    Create 10.Flatten a list.py
    challenge 10

---
+++ python challenges by  Python Principles/10.Flatten a list.py
@@ -0,0 +1,43 @@
+"""
+Flatten a list
+Write a function that takes a list of lists and flattens it into a one-dimensional list.
+
+Name your function flatten. It should take a single parameter and return a list.
+
+For example, calling:
+
+flatten([[1, 2], [3, 4]])
+Should return the list:
+
+[1, 2, 3, 4]
+"""
+
+#my solution............................
+def flatten(l):
+    k=[]
+    for i in l:
+        for j in i :
+            k.append(j)
+    return k
+    
+flatten([[1, 2], [3, 4]])
+
+........................................
+
+#solution....................................
+# naive solution
+def flatten(outer_list):
+    result = []
+    for inner_list in outer_list:
+        for item in inner_list:
+            result.append(item)
+    return result
+
+# solution with nested list comprehensions
+# (can be put on a single line for conciseness)
+def flatten(outer_list):
+    return [
+        item
+        for inner_list in outer_list
+        for item in inner_list
+    ]
